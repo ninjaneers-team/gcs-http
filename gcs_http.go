1b611ce5eb798b310ce02f4cfddfe51b01400d29
Claus Strasburger <claus@strasburger.de>
2019-08-13 10:49:41 +0200

    support basic auth

--- gcs_http.go
+++ gcs_http.go
@@ -4,47 +4,102 @@ package p
 import (
 	"cloud.google.com/go/storage"
 	"context"
+	"crypto/subtle"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
-var ctx context.Context
+var baseCtx context.Context
 var client *storage.Client
 var bucket *storage.BucketHandle
 
+var authUsers map[string]string
+
 func init() {
 	var err error
-	ctx = context.Background()
-	client, err = storage.NewClient(ctx)
-	check(err, "creating client")
+	baseCtx = context.Background()
+	client, err = storage.NewClient(baseCtx)
+	p(err, "creating client")
 
 	bucket = client.Bucket(os.Getenv("BUCKET"))
+
+	basicAuth := os.Getenv("BASICAUTH")
+	if basicAuth != "" {
+		items := strings.Split(basicAuth, " ")
+		authUsers = make(map[string]string)
+		for _, pair := range items {
+			pair = strings.TrimSpace(pair)
+			pairSplit := strings.SplitN(pair, ":", 2)
+			if len(pairSplit) != 2 {
+				panic("Basicauth format: 'user:pass user2:pass2...'")
+			}
+			authUsers[pairSplit[0]] = pairSplit[1]
+		}
+		log.Println("Allowed users: %+v", authUsers)
+	}
 }
 
-func check(err error, why string) {
+func p(err error, why string) {
 	if err != nil {
 		log.Panic(why, err)
 	}
 }
 
-// HelloWorld prints the JSON encoded "message" field in the body
-// of the request or "Hello, World!" if there isn't one.
+func p2(cancel context.CancelFunc, w http.ResponseWriter, err error, why string, code int) bool {
+	if err != nil {
+		http.Error(w, fmt.Sprintf("%s: %s", why, err), code)
+		cancel()
+		return true
+	}
+	return false
+}
+
 func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithCancel(baseCtx)
+	defer cancel()
+	if authUsers != nil && !BasicAuth(w, r, authUsers) {
+		return
+	}
+	ServeFile(ctx, cancel, w, r)
+}
+
+func BasicAuth(w http.ResponseWriter, r *http.Request, users map[string]string) bool {
+	user, expected, ok := r.BasicAuth()
+	if !ok {
+		//WWW-Authenticate: Basic realm="RealmName"
+		w.Header().Add("WWW-Authenticate", "Basic realm=\"Please log in\"")
+		w.WriteHeader(http.StatusUnauthorized)
+		return false
+	}
+	pwd, ok := users[user]
+	return subtle.ConstantTimeCompare([]byte(pwd), []byte(expected)) == 1
+}
+
+func ServeFile(ctx context.Context, cancel context.CancelFunc, w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path[1:]
-	myCtx := context.WithValue(ctx, "path", path)
+	ctx = context.WithValue(ctx, "path", path)
 
 	log.Println("Object path:", path)
 	obj := bucket.Object(path)
-	reader, err := obj.NewReader(myCtx)
+	reader, err := obj.NewReader(ctx)
 	if err == storage.ErrObjectNotExist {
-		http.Error(w, "Not found", 404)
+		p2(cancel, w, err, "Not found", http.StatusNotFound)
 		return
 	}
-	check(err, "finding object")
-	defer reader.Close()
+	if p2(cancel, w, err, "finding object", http.StatusNotFound) {
+		return
+	}
+	defer func() {
+		err := reader.Close()
+		p2(cancel, w, err, "closing output", http.StatusInternalServerError)
+	}()
 
 	_, err = io.Copy(w, reader)
-	check(err, "serving data")
+	if p2(cancel, w, err, "streaming object", http.StatusInternalServerError) {
+		return
+	}
 }
